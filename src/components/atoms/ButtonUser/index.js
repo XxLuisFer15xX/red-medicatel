ccfda103fa8b03bd8686091647d2cdc2ffcf8c74
Luis Fernando Solano Martínez <luisfer.sm15@gmail.com>
2022-06-16 17:17:58 -0600

    :lipstick: styles: Design of the Page Not Found Screen

--- src/components/atoms/ButtonUser/index.js
+++ src/components/atoms/ButtonUser/index.js
@@ -3,15 +3,15 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import UserLogo from '../../../assets/images/user-logo.PNG';
 import TextCustom from '../TextCustom';
 
-const ButtonUser = ({ className = ''}) => {
+const ButtonUser = ({ className = '', textStyle = '', iconColor = 'primary'}) => {
   return (
     <div className={`flex items-center ${className}`}>
       <img src={UserLogo} className={'h-8 mr-4 w-10'} alt="user" />
       <TextCustom
         text="Fundación Grupo Terra"
-        className="mr-4 text-sm text-blue-800"
+        className={`mr-4 text-sm text-blue-800 ${textStyle}`}
       />
-      <KeyboardArrowDownIcon color="primary" />
+      <KeyboardArrowDownIcon color={iconColor} />
     </div>
   );
 };
--- src/components/pages/PageNotFound/index.js
+++ src/components/pages/PageNotFound/index.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// Hooks
+import AuthContext from '../../../hooks/context/authContext';
+
+// Components
+import { ButtonCustom, TextCustom } from '../../atoms';
 
 const PageNotFound = () => {
+  const navigate = useNavigate();
+  const { auth } = useContext(AuthContext);
+
+  const handleGoBack = () => {
+    if (auth.isLogin) {
+      navigate('/dashboard/home');
+    } else {
+      navigate('/login');
+    }
+  };
+
   return (
-    <div>Page not found</div>
-  )
-}
+    <div className="h-screen w-full bg-red-600 flex justify-center items-center">
+      <div className="flex flex-col items-center">
+        <TextCustom
+          text="Página no encontrada"
+          variant="h2"
+          className="text-white"
+        />
+        <ButtonCustom
+          text="¿Regresar al Dashboard?"
+          variant="text"
+          className="text-white text-xl"
+          onClick={handleGoBack}
+          style={{textTransform: 'none'}}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default PageNotFound;

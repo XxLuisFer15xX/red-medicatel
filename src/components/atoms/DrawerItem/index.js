549a453cf95faac81f10c44ec712ee7d92d06616
Luis Fernando Solano Martínez <luisfer.sm15@gmail.com>
2022-06-15 19:54:00 -0600

    :sparkles: feat: Functionality to logout and redesign side menu.

--- src/components/atoms/DrawerItem/index.js
+++ src/components/atoms/DrawerItem/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
 
-const DrawerItem = ({ text = '', index = 0, onClick = () => null }) => {
+const DrawerItem = ({ text = '', onClick = () => null, icon = null }) => {
   const handleClick = () => {
     onClick();
-  }
+  };
 
   return (
     <ListItem key={text} disablePadding onClick={handleClick}>
       <ListItemButton>
-        <ListItemIcon>
-          {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-        </ListItemIcon>
+        <ListItemIcon>{icon}</ListItemIcon>
         <ListItemText primary={text} />
       </ListItemButton>
     </ListItem>
--- src/components/molecules/DrawerContent/index.js
+++ src/components/molecules/DrawerContent/index.js
@@ -1,33 +1,67 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Divider from '@mui/material/Divider';
 import List from '@mui/material/List';
 import Toolbar from '@mui/material/Toolbar';
+import { useNavigate } from 'react-router-dom';
+
+// Hooks
+import AuthContext from '../../../hooks/context/authContext';
+
+// Icons
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import Logout from '@mui/icons-material/Logout';
+
+// Components
 import { DrawerItem, TextCustom } from '../../atoms';
-import { apiTest } from '../../../api';
+
+// Const
+import { authTypes } from '../../../commons/types';
 
 const DrawerContent = () => {
-  const handlePressItem = async () => {
-    const response = await apiTest();
-    console.log(response);
-  }
+  const navigate = useNavigate();
+  const { dispatchAuth } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    dispatchAuth({ type: authTypes.logout });
+  };
+
+  const handleEmpresa = () => {
+    navigate('/dashboard/home');
+  };
+
+  const handleColaboradores = () => {
+    navigate('/dashboard/colaborators');
+  };
 
   return (
-    <div>
+    <div className="flex flex-col h-full">
       <Toolbar>
-        <TextCustom text={'Asegurame'} variant={'h4'} />
+        <TextCustom text={'Red Medicatel'} variant={'h4'} />
       </Toolbar>
       <Divider />
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <DrawerItem key={index} text={text} index={index} onClick={handlePressItem} />
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <DrawerItem key={index} text={text} index={index} onClick={handlePressItem} />
-        ))}
-      </List>
+      <div className="flex flex-col justify-between h-full">
+        <List>
+          <DrawerItem
+            text={'Empresa'}
+            onClick={handleEmpresa}
+            icon={<InboxIcon />}
+          />
+          <DrawerItem
+            text={'Colaboradores'}
+            onClick={handleColaboradores}
+            icon={<MailIcon />}
+          />
+        </List>
+        <div>
+          <Divider />
+          <DrawerItem
+            text={'Cerrar sesión'}
+            onClick={handleLogout}
+            icon={<Logout />}
+          />
+        </div>
+      </div>
     </div>
   );
 };

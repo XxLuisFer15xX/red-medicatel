f280b863414c4e793bdc2cad26d2f76618745c67
Luis Fernando Solano Martínez <luisfer.sm15@gmail.com>
2022-06-16 16:44:38 -0600

    :lipstick: styles: fix design of the button in header menu

--- src/components/molecules/HeaderMenu/index.js
+++ src/components/molecules/HeaderMenu/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import AddIcon from '@mui/icons-material/Add';
 import { ButtonCustom, ButtonUser } from '../../atoms';
 
 const HeaderMenu = ({ onClick = () => null}) => {
   return (
     <div className="h-24 pr-20 w-full flex flex-row-reverse items-start">
       <ButtonUser className='h-10' />
-      <ButtonCustom text='Agregar' className='h-10 mr-10 w-auto bg-blue-500' />
+      <ButtonCustom text='Agendar' className='h-10 mr-10 w-auto bg-blue-500' startIcon={<AddIcon />} />
     </div>
   );
 };
